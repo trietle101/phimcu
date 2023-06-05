067731a1ccb5c2f6725bba216f04fd6c2a85124b
MAYTINHCUATRIET\triet <trietle101@gmail.com>
2023-06-05 12:04:01 +0700

    api call

--- script/src.js
+++ script/src.js
@@ -1,55 +1,61 @@
-import axios from "../node_modules/axios/dist/node/axios.cjs";
-// const headerContainer = document.querySelector("header .container");
-// export default async function getMovie(id) {
-//     try {
-//         await axios
-//             .get(
-//                 `https://api.themoviedb.org/3/movie/${id}?api_key=4a1fbd255d069fb6c571f7e1aae6f88b`
-//             )
-//             .then((res) => {
-//                 headerContainer.innerHTML += `
-//                 <div class="nav__info">
-//                     <h1>${res.data.original_title}</h1>
-//                     <p>res.data.overview</p>
-//                     <div class="nav__button">
-//                         <a href="#">
-//                             <i class="fa-solid fa-play"></i>
-//                             <p>Play</p>
-//                         </a>
-//                         <a href="html/index.html">
-//                             <i class="fa-solid fa-circle-info"></i>
-//                             <p>More info</p>
-//                         </a>
-//                     </div>
-//                 </div>
-//             `;
-//                 console.log(res.data);
-//             });
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-// getMovie(164);
+const key = "4a1fbd255d069fb6c571f7e1aae6f88b";
 
-// https://api.themoviedb.org/3/movie/popular?language=en-US&page=1'
-// async function getMoviePop() {
-//     await axios
-//         .get(
-//             `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1?api_key=4a1fbd255d069fb6c571f7e1aae6f88b`
-//         )
-//         .then((res) => {
-//             console.log(res.data);
-//         });
-// }
-// getMoviePop();
+const headerContainer = document.querySelector("header .container");
+const popMoviesContainer = document.querySelector(
+    "main section:first-child .cover"
+);
 
-async function getMovieAct() {
+async function getMovie(id) {
+    try {
+        await axios
+            .get(
+                `https://api.themoviedb.org/3/movie/${id}?api_key=${key}&append_to_response=credits,images,videos`
+            )
+            .then((res) => {
+                headerContainer.innerHTML += `
+                    <div class="nav__info">
+                        <h1>${res.data.original_title}</h1>
+                        <p>${res.data.overview}</p>
+                        <div class="nav__button">
+                            <a href="#">
+                                <i class="fa-solid fa-play"></i>
+                                <p>Play</p>
+                            </a>
+                            <a href="html/detail.html">
+                                <i class="fa-solid fa-circle-info"></i>
+                                <p>More info</p>
+                            </a>
+                        </div>
+                    </div>
+                `;
+            });
+    } catch (error) {
+        console.log(error);
+    }
+}
+getMovie(164);
+
+async function getMoviePop() {
     await axios
         .get(
-            `https://api.themoviedb.org/3/movie/164/credits?api_key=4a1fbd255d069fb6c571f7e1aae6f88b`
+            `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1?`
         )
         .then((res) => {
-            console.log(res.data.cast);
+            const popMovies = res.data.results.slice(0, 6);
+            popMovies.forEach((movie) => {
+                popMoviesContainer.innerHTML += `<img src="https://image.tmdb.org/t/p/original${movie.poster_path}" />`;
+            });
         });
 }
-getMovieAct();
+getMoviePop();
+
+// async function getMovieAct() {
+//     await axios
+//         .get(
+//             `https://api.themoviedb.org/3/person/popular?language=en-US&page=1?api_key=${key}`
+//         )
+//         .then((res) => {
+//             console.log(res.data.cast);
+//         });
+// }
+// getMovieAct();
